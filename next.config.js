dcedb804191f763c3cfe624d941d0e457095a1a3
waltiu <waltiu@163.com>
2022-04-06 16:32:50 +0800

    test：build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
+const pkg =require('./package.json')
 const nextConfig = {
+  assetPrefix:pkg.name,
   reactStrictMode: true,
   images: {
     loader: "custom",
   },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.output.publicPath='always-site'
     return config;
   },
   exportPathMap: async function (
