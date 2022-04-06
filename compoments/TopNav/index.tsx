5043a85d15b4ddb4e0b749a5e45e1efa81d96865
waltiu <waltiu@163.com>
2022-04-06 17:30:14 +0800

    feat: sass

--- compoments/TopNav/index.tsx
+++ compoments/TopNav/index.tsx
@@ -1,5 +1,12 @@
+import { Input } from 'antd';
+import styles from './index.module.scss'
+console.log(styles,'styles')
 const TopNav = ()=>{
-    return <div>11</div>
+    return <div className={styles['top-nav']}>
+        <div className={styles['search-box']}>
+        <Input className={styles.search} placeholder="Borderless" bordered={false} />
+        </div>
+    </div>
 }
 
 export default TopNav
