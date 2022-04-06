2c2ace3f5e8871eef1d69732fc51a015c112267f
waltiu <waltiu@163.com>
2022-04-06 16:35:05 +0800

    feat: 环境变量进行环境区分

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const pkg =require('./package.json')
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
-  assetPrefix:pkg.name,
+  assetPrefix:isProd?pkg.name:'',
   reactStrictMode: true,
   images: {
     loader: "custom",
