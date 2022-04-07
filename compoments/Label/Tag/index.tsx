f6c0c45fbe4a7f115e4b14161d2080104b74bb34
waltiu <waltiu@163.com>
2022-04-07 14:39:26 +0800

    chore: 添加左侧tagList

--- compoments/Label/Tag/index.tsx
+++ compoments/Label/Tag/index.tsx
@@ -1,13 +1,17 @@
-import { type } from "os"
 import { FC } from "react"
+import styles from './index.module.scss'
+import Image from '../../../compoments/Image'
 
 type TagPropsType={
     data:any
 }
 
 const Tag:FC<TagPropsType> =({data})=>{
-    return <div>
-        {data.name}
+    return <div className={styles.tag}>
+                <Image src={data.icon}  alt=""  width={20} height={20}/>
+            <div className={styles.name}>
+                <a href={data.link} target="_blank" rel="noreferrer" > {data.name}</a>
+            </div>
     </div>
 }
 export default Tag
--- compoments/Label/index.tsx
+++ compoments/Label/index.tsx
@@ -1,25 +1,29 @@
-import useTag from '../../composables/useTag'
-import Tag from './Tag'
-import {sourceTypes} from '../../static/constant'
-import {TagType} from '../../types/label'
-import styles from './index.module.scss'
-const Label=()=>{
-    const [tags,operTag]=useTag()
-    console.log(tags,'tags',operTag)
-    return (
-        <div>
-            {
-                (tags||[]).map(([type,value]:[string,[TagType]])=>{
-                    return <div key={type}>
-
-                        <div>{sourceTypes[type as keyof typeof sourceTypes ]?.title||'其他'}</div>
-                        <div>{
-                            value?.length&&value.map((item:TagType)=><Tag key={item.name} data={item}/>)
-                            }</div>
-                    </div>
-                })
-            }
-        </div>
-    )
-}
-export default Label
+import useTag from "../../composables/useTag";
+import Tag from "./Tag";
+import { sourceTypes } from "../../static/constant";
+import { TagType } from "../../types/label";
+import styles from "./index.module.scss";
+const Label = () => {
+  const [tags, operTag] = useTag();
+  console.log(tags, "tags", operTag);
+  return (
+    <div>
+      {(tags || []).map(([type, value]: [string, [TagType]]) => {
+        return (
+          <div key={type} className={styles["label-box"]}>
+            <div className={styles.title}>
+                {sourceTypes[type as keyof typeof sourceTypes]?.title || "其他"}
+            </div>
+            <div className={styles.tags}>
+              {value?.length &&
+                value.map((item: TagType) => (
+                  <Tag key={item.name} data={item} />
+                ))}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+export default Label;
