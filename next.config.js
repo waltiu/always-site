78c9b68eded8cdf08a45d5cc53cfdbd02afdfd79
waltiu <waltiu@163.com>
2022-04-08 17:26:54 +0800

    chore: 删减next配置

--- next.config.js
+++ next.config.js
@@ -4,20 +4,8 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   assetPrefix:isProd?`/${pkg.name}/`:'',
-  reactStrictMode: true,
   images: {
     loader: "custom",
-  },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    return config;
-  },
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      "/": { page: "/" },
-    };
   },
 };
 
