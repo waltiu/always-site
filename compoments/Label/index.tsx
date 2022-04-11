e0ce52894a9d0b3ea0e9c8da5706aee05b999dad
waltiu <waltiu@163.com>
2022-04-11 18:38:19 +0800

    feat: 添加tab拖拽

--- compoments/Label/index.tsx
+++ compoments/Label/index.tsx
@@ -1,4 +1,5 @@
 import useTag from "composables/useTag";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import Tag from "./Tag";
 import { sourceTypes, IS_HOT } from "static/constant";
 import { TagType } from "types/label";
@@ -11,41 +12,71 @@ const Label = () => {
   const [tags, operTag] = useTag();
   return (
     <div>
-      {(tags || []).map(([type, value]: [string, [TagType]]) => {
-        return (
-          <div
-            key={type}
-            className={classNames(
-              styles["label-box"],
-              CardStyle.card,
-              styles[type === IS_HOT ? "is-hot" : ""]
-            )}
-          >
-            {type === IS_HOT ? (
-              <>
-                <div className={styles.hot}>
-                  <Image src={hotImg} alt="" />
-                </div>
-              </>
-            ) : (
-              <>
-                <div className={styles.title}>
-                  {sourceTypes[type as keyof typeof sourceTypes]?.title ||
-                    "其他"}
-                </div>
-                <div className={styles.oper}>11 </div>
-              </>
-            )}
-
-            <div className={styles.tags}>
-              {value?.length &&
-                value.map((item: TagType) => (
-                  <Tag key={item.name} data={item} />
-                ))}
+      <DragDropContext
+        onDragEnd={(value) => {
+          console.log(value, "value");
+        }}
+      >
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <div       {...provided.droppableProps}
+            ref={provided.innerRef}>
+              {(tags || []).map(
+                ([type, value]: [string, [TagType]], index: number) => {
+                  return (
+                    <Draggable key={type} draggableId={type} index={index}>
+                      {(provided) => {
+                        return (
+                          <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                            key={type}
+                            className={classNames(
+                              styles["label-box"],
+                              CardStyle.card,
+                              styles[type === IS_HOT ? "is-hot" : ""]
+                            )}
+                          >
+                            {type === IS_HOT ? (
+                              <>
+                                <div className={styles.hot}>
+                                  <Image src={hotImg} alt="" />
+                                </div>
+                              </>
+                            ) : (
+                              <>
+                                <div className={styles.title}>
+                                  {sourceTypes[type as keyof typeof sourceTypes]
+                                    ?.title || "其他"}
+                                </div>
+                                <div className={styles.oper}>11 </div>
+                              </>
+                            )}
+  
+                            <div className={styles.tags}>
+                              {value?.length &&
+                                value.map((item: TagType) => (
+                                  <div
+                                  key={item.name}
+                            
+                                  >
+                                  <Tag key={item.name} data={item} />
+  
+                                  </div>
+                                ))}
+                            </div>
+                          </div>
+                        )
+                      }}
+                    </Draggable>
+                  );
+                }
+              )}
             </div>
-          </div>
-        );
-      })}
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 };
