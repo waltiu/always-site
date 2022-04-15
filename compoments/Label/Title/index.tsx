5662115e43ec45895989f05d6b86788c20dbf8a8
waltiu <waltiu@163.com>
2022-04-15 23:37:11 +0800

    chore: 自动聚焦输入框

--- compoments/Label/Title/index.tsx
+++ compoments/Label/Title/index.tsx
@@ -1,43 +1,63 @@
-import { LabelType,SetLabelMethodType } from "types/label";
+import { LabelType, SetLabelMethodType } from "types/label";
 import { OTHER_LABEL } from "static/constant";
 import styles from "./index.module.scss";
-import { useState,FocusEvent,KeyboardEvent, useRef } from "react";
-import { Input,InputRef } from "antd";
-const LabelTitle = ({ label,labels,operLabel }: {
-    label:LabelType,
-    labels:LabelType[]
-    operLabel:SetLabelMethodType
+import { useState, FocusEvent, KeyboardEvent, useRef } from "react";
+import { Input, InputRef } from "antd";
+const LabelTitle = ({
+  label,
+  labels,
+  operLabel,
+}: {
+  label: LabelType;
+  labels: LabelType[];
+  operLabel: SetLabelMethodType;
 }) => {
+
   const { type } = label;
-  const [isInput,setIsInput]=useState(false)
-  const inputRef=useRef<InputRef>(null)
-  const changeLabelName=(name:string)=>{
-    const currentLabelIndex=labels.findIndex(item=>item.type===type)
-    if(currentLabelIndex>0){
-        const newLabels=[...labels]
-        newLabels[currentLabelIndex].type=name
-        operLabel(newLabels)
-        setIsInput(false)
+
+  const [isInput, setIsInput] = useState(false);
+  const inputRef = useRef<InputRef>(null);
+
+  const changeLabelName = (name: string) => {
+    const currentLabelIndex = labels.findIndex((item) => item.type === type);
+    if (currentLabelIndex > 0) {
+      const newLabels = [...labels];
+      newLabels[currentLabelIndex].type = name;
+      operLabel(newLabels);
+      setIsInput(false);
     }
-  }
+  };
+
   return (
     <>
       <div className={styles.title}>
-          {isInput?<Input bordered={false} ref={inputRef}  defaultValue={type} onBlur={(e:FocusEvent<HTMLInputElement, Element>)=>{
-            changeLabelName(e.target.value)
-          }}
-          onPressEnter={()=>{
-                inputRef.current?.blur();
-          }}
+        {isInput ? (
+          <Input
+            bordered={false}
+            ref={inputRef}
+            defaultValue={type}
+            onBlur={(e: FocusEvent<HTMLInputElement, Element>) => {
+              changeLabelName(e.target.value);
+            }}
+            onPressEnter={() => {
+              inputRef.current?.blur();
+            }}
           />
-          :  <span  onDoubleClick={()=>{
-          setIsInput(true)
-      }} >
-          {type || OTHER_LABEL}
-          </span>}
-        
+        ) : (
+          <span
+            onDoubleClick={() => {
+              setIsInput(true);
+              setTimeout(() => {
+                inputRef.current?.focus();
+              }, 0);
+            }}
+          >
+            {type || OTHER_LABEL}
+          </span>
+        )}
       </div>
     </>
   );
 };
+
 export default LabelTitle;
