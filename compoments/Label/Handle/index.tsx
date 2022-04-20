667b8e6bb29fa96c3ddf893da5f2839d3a442cbf
waltiu <waltiu@163.com>
2022-04-20 13:48:27 +0800

    chore: 替换icon

--- compoments/Label/Handle/index.tsx
+++ compoments/Label/Handle/index.tsx
@@ -1,4 +1,4 @@
-import { CloseOutlined, MoreOutlined,PlusOutlined,EditOutlined,MinusOutlined } from "@ant-design/icons";
+import { CloseOutlined, MoreOutlined,PlusOutlined,DeleteRowOutlined,InsertRowBelowOutlined } from "@ant-design/icons";
 import classNames from "classnames";
 import { SetLabelMethodType } from "types/label";
 import styles from "./index.module.scss";
@@ -6,23 +6,24 @@ const Handle = ({isOpen,index,type,setIsOperingType,operLabel}:{isOpen:boolean,i
   return (
     <div className={classNames(styles.oper, isOpen ? styles["is-open"] : "")}>
       <div className={styles.detail}>
-        <div className={styles.icon} onClick={()=>{
+      <div className={styles.icon} onClick={()=>{
           operLabel(index,'addLabel')
         }}>
+          <span>
+          <InsertRowBelowOutlined />
+          </span>
+        </div>
+        <div className={styles.icon} >
           <span>
           <PlusOutlined />
           </span>
         </div>
-        <div className={styles.icon}>
-          <span>
-          <EditOutlined />
-          </span>
-        </div>
+      
         <div className={styles.icon} onClick={()=>{
           operLabel(type,'deleteLabel')
         }}>
           <span>
-          <MinusOutlined  />
+          <DeleteRowOutlined />
           </span>
         </div>
       </div>
